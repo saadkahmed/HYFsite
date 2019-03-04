2045ff251dcc42586787d00dc56fe4bfa387e0c5
Saad Ahmed <skahmed@ryerson.ca>
2019-03-04 16:39:15 -0500

    adding new js stuff

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("poop");
+
+function buttonPress() {
+    console.log("poop");
+    document.getElementById('btnInstructor').style.display = "block";
+
+}
+
+function showDiv() {
+    console.log("poop");
+    document.getElementById('comingSoon').style.display = "block";
+}
+
