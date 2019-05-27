139de521a1611d07a54bda2594692f15923aae9b
Saad Ahmed <skahmed@ryerson.ca>
2019-05-27 12:52:32 -0400

    changed styling and fixed form submission

--- js/index.js
+++ js/index.js
@@ -6,8 +6,29 @@ function buttonPress() {
 
 }
 
-function showDiv() {
-    console.log("poop");
-    document.getElementById('comingSoon').style.display = "block";
-}
 
+$(document).ready(function () {
+    $('.submit').click(function(event){
+
+        var email = $('.email').val()
+        var name = $('.name').val()
+        var topic = $('.topic').val()
+        var message = $('.message').val()
+        var status = $('.status')
+        status.empty()
+
+        console.log('form button clicked', email, name, message, topic);
+
+        if(email.length > 5 && email.includes('@') && email.includes('.')){
+            status.append('<div>email is valid</div>')       
+        }else{
+            status.append('<div>email is not valid</div>')
+            event.preventDefault();        
+        }
+
+        if(message.length < 20){
+            status.append('<div>message must be longer than 20 characters</div>') 
+            event.preventDefault();         }else{      
+        }
+    })
+})
